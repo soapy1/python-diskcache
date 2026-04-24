fccb14852574151ecf1f4615ec7fa6e5135323b7
sophia <scastellarin@quansight.com>
2026-04-24 10:25:41 -0700

    Mitigate CVE-2025-69872
    - create cache dirs only accessible by owner
    - set default cache to JSONCache
    
    refs:
    - https://github.com/grantjenks/python-diskcache/pull/359
    - src.fedoraproject.org/rpms/python-diskcache/pull-request/1#

--- diskcache/core.py
+++ diskcache/core.py
@@ -232,7 +232,7 @@ class Disk:
 
         for count in range(1, 11):
             with cl.suppress(OSError):
-                os.makedirs(full_dir)
+                os.makedirs(full_dir, 0o700)
 
             try:
                 # Another cache may have deleted the directory before
@@ -417,7 +417,7 @@ def args_to_key(base, args, kwargs, typed, ignore):
 class Cache:
     """Disk and file backed cache."""
 
-    def __init__(self, directory=None, timeout=60, disk=Disk, **settings):
+    def __init__(self, directory=None, timeout=60, disk=JSONDisk, **settings):
         """Initialize cache instance.
 
         :param str directory: cache directory
@@ -444,7 +444,7 @@ class Cache:
 
         if not op.isdir(directory):
             try:
-                os.makedirs(directory, 0o755)
+                os.makedirs(directory, 0o700)
             except OSError as error:
                 if error.errno != errno.EEXIST:
                     raise EnvironmentError(
